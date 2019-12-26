e6b6977d37ca00a47be0ea9c9a9d8b49866b341a
Manraj <chotanattu@gmail.com>
2019-12-26 15:03:54 +0530

    set up the layout and partials

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const router = require('./routes/index');
+
+
+app.use(expressLayouts);
 
 // use express router
-app.use('/', require('./routes/index'));
+app.use('/', router);
 
 // Setting up ejs as the view engine
 app.set('view engine', 'ejs');
