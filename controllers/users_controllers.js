263eed37855dcc95029c536cf798d824e20bb907
Manraj <chotanattu@gmail.com>
2019-12-26 17:52:28 +0530

    set up routes for the signIn page and the signUp page

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -2,4 +2,28 @@ module.exports.profile = function(req, res) {
     return res.render('user_profile', {
         title: "User Profile Page"
     });
+};
+
+// render the signUp page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Codeial | SignUp"
+    })
+};
+
+// render the signIn page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | SignIn"
+    })
+};
+
+// get the signUp data
+module.exports.create = function(req, res){
+    // ToDo later
+}
+
+// sign in and create a session for the user 
+module.exports.createSession = function(req, res){
+    // ToDo later
 }
