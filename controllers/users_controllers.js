10061cfbbe1fa6820d09955302991b13d4f8258a
Manraj <chotanattu@gmail.com>
2019-12-26 20:54:40 +0530

    add controller action for the signUp functionality in the web app

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res) {
     return res.render('user_profile', {
         title: "User Profile Page"
@@ -20,10 +22,32 @@ module.exports.signIn = function(req, res){
 
 // get the signUp data
 module.exports.create = function(req, res){
-    // ToDo later
-}
+    
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email : req.body.email}, function(err, user){
+        
+        if(err) console.log('err in finding user in signing up');
+
+        if(!user){
+            // even if we send additional fields they would not get added in the schema
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            });
+        } else{
+            return res.redirect('back');
+        }
+    });
+    
+};
 
 // sign in and create a session for the user 
 module.exports.createSession = function(req, res){
     // ToDo later
-}
+};
