509e8efe6757c0ff7a2741773d75530f8adaf2cc
Manraj <chotanattu@gmail.com>
2019-12-26 18:21:54 +0530

    set up cookieParser access and modify cookies

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const expressLayouts = require('express-ejs-layouts');
 const router = require('./routes/index');
 const db = require('./config/mongoose');
 
+// for reading and writing into cookies we will be using a package- cookie-parser
+const cookieParser = require('cookie-parser');
+
+// for reading the data sent by post requests
+app.use(express.urlencoded());
+
+// for reading and modifying the cookies
+app.use(cookieParser());
+
 // If you use assets then, in the layout.ejs file you need to provide the path of the file relative to the assets folder
 app.use(express.static('./assets'));
 
+// using the layout.ejs file
 app.use(expressLayouts);
 
 // Extract styles and scripts from subpages into the layout
