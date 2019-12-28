0272ecf8f317b42da712aabb644be5d50184df1b
Manraj <chotanattu@gmail.com>
2019-12-28 17:01:51 +0530

    pre-populate the user so that user info can be displayed while displaying the posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,7 +4,25 @@ module.exports.home = function(req, res) {
     // return res.end('<h1>Express is up for Codeial</h1>');
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    Post.find({}, function(err, posts){
+
+    // without populating the user object
+    // Post.find({}, function(err, posts){
+    //     if(err){
+    //         console.log('Error in finding posts');
+    //         return;
+    //     }
+    //     if(!posts){
+    //         console.log('There seems to be no posts in the db');
+    //         return;
+    //     }
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
         if(err){
             console.log('Error in finding posts');
             return;
@@ -18,7 +36,6 @@ module.exports.home = function(req, res) {
             posts: posts
         });
     });
-    
-};
 
+};
 // module.exports.actionName = function(req, res){}
