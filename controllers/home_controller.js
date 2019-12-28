d4f3feb2b22a28e4b9c487eb3513859d3103203a
Manraj <chotanattu@gmail.com>
2019-12-28 15:56:46 +0530

    display posts on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res) {
     // return res.end('<h1>Express is up for Codeial</h1>');
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+    Post.find({}, function(err, posts){
+        if(err){
+            console.log('Error in finding posts');
+            return;
+        }
+        if(!posts){
+            console.log('There seems to be no posts in the db');
+            return;
+        }
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
-}
+    
+};
 
 // module.exports.actionName = function(req, res){}
