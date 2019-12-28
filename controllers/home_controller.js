c52bb5a2c45bff26b4a1000466c5b98e23354788
Manraj <chotanattu@gmail.com>
2019-12-28 17:13:37 +0530

    add check auth when creating new post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,23 +4,7 @@ module.exports.home = function(req, res) {
     // return res.end('<h1>Express is up for Codeial</h1>');
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
-
-    // without populating the user object
-    // Post.find({}, function(err, posts){
-    //     if(err){
-    //         console.log('Error in finding posts');
-    //         return;
-    //     }
-    //     if(!posts){
-    //         console.log('There seems to be no posts in the db');
-    //         return;
-    //     }
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts: posts
-    //     });
-    // });
-
+    
     // populate the user of each post
     Post.find({}).populate('user').exec(function(err, posts){
         if(err){
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-
 const postsController = require('../controllers/posts_controller');
 
-router.post('/create', postsController.create);
+router.post('/create', require('../config/passport-local-strategy').checkAuthentication, postsController.create);
 
 module.exports = router;
