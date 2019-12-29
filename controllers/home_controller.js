1e111726d4213163159d44e7db85de53577de390
MANRAJSINGH007 <chotanattu@gmail.com>
2019-12-29 20:08:10 +0530

    display the comments on the browser with the posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -6,7 +6,15 @@ module.exports.home = function(req, res) {
     // res.cookie('user_id', 25);
     
     // populate the user of each post
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         if(err){
             console.log('Error in finding posts');
             return;
