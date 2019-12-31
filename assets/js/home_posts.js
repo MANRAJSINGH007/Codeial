16a35150ec7e7f8d7fb2717e7a6f268032c655e2
MANRAJSINGH007 <manrajs2014@gmail.com>
2019-12-31 16:54:05 +0530

    create post using jquery and ajax and display on home page

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -12,7 +12,8 @@
                 data: newPostForm.serialize(), 
                 success: function(data) {
                     // we will receive some data
-                    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
                 },
                 error: function(error){
                     console.log(error.responseText);
@@ -22,6 +23,32 @@
     }
 
     createPost();
-    
+
     // method to create a post in DOM
+    let newPostDom = (post) => {
+        // backticks to interpolate strings ES6 feature
+        return $(`<li id="post-${post._id}">
+        <p>
+            <small>
+                <a class="delete-post-button" href="/posts/destroy/${post.id}">Delete </a>
+            </small>
+                ${post.content}
+            <br>
+            <small>
+                ${post.user.name}
+            </small>
+        </p>
+        <div class="post-comments">
+            <form action="/comment/create" method="POST">
+                <input type="text" name="content" placeholder="Type here to add comment..." required>
+                <input type="hidden" name="post" value="${post._id}">
+                <input type="submit" value="Add Comment">
+            </form>
+            <div class="post-comments-list">
+                <ul id="post-comments-${post._id}">
+                </ul>
+            </div>
+        </div>
+    </li>`);
+    }
 }
