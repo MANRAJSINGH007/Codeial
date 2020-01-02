1665df8281df7d8d373ab2f6c01645185139ae88
MANRAJSINGH007 <manrajs2014@gmail.com>
2020-01-02 21:58:46 +0530

    add upload profile photo feature to form on profile page

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -9,15 +9,41 @@ module.exports.profile = function(req, res) {
     });
 };
 
-module.exports.update = function(req, res) {
+module.exports.update = async function(req, res) {
+    // if(req.user.id == req.params.id) {
+    //     User.findByIdAndUpdate(req.params.id, {
+    //         name : req.body.name,
+    //         email: req.body.email
+    //     }, (err, user) => {
+    //         return res.redirect('back');
+    //     });
+    // } else {
+    //     return res.status(401).send('Unauthorized');
+    // }
     if(req.user.id == req.params.id) {
-        User.findByIdAndUpdate(req.params.id, {
-            name : req.body.name,
-            email: req.body.email
-        }, (err, user) => {
-            return res.redirect('back');
-        });
+        try {
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err) {
+                if(err) {
+                    console.log('*****Multer Error: ', err);
+                    return;
+                }
+                // console.log(req.file);
+                user.name = req.body.name;
+                user.email = req.body.email;
+                if(req.file) {
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+        } catch(err) {
+            req.flash('error', err);
+            res.redirect('back');
+        }
     } else {
+        req.flash('error', err);
         return res.status(401).send('Unauthorized');
     }
 };
